92cd61073a6c8a7bd8b0d56bb51a847382cd0650
Pawel Godula <bolomaster7@gmail.com>
2023-04-27 13:28:28 +0200

    redux changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 // import Footer from './components/views/Footer/Footer';
 // import Header from './components/views/Header/Header';
-// import AddPage from './components/pages/AdPage/AdPage';
-// import EditAds from './components/features/EditAds/EditAds';
-// import Login from './components/pages/Login/Login';
-// import Register from './components/pages/Register/Register';
-// import Logout from './components/pages/Logout/Logout';
-// import AddAds from './components/features/AddAds/AddAds';
-// import Search from './components/pages/Search/Search'
+import AdPage from './components/pages/AdPage/AdPage';
+import EditAds from './components/features/EditAds/EditAds';
+import Login from './components/pages/Login/Login';
+import Register from './components/pages/Register/Register';
+import Logout from './components/pages/Logout/Logout';
+import AdAds from './components/features/AdAds/AdAds';
+import Search from './components/pages/Search/Search'
 
 const App = () => {
   return (
@@ -18,13 +18,13 @@ const App = () => {
       <Container>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/ads/:id" element={<AddPage />} />
-          <Route path="/addAds" element={<AddAds />} />
-          <Route path="/ads/edit/:id" element={<EditAds />} />
+          {/* <Route path="/ads/:id" element={<AdPage />} />
+          <Route path="/adAds" element={<AdAds />} />
+          <Route path="/ads/edit/:id" element={<EditAds />} /> */}
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/logout" element={<Logout />} />
-          <Route path="/search/:searchId" element={<Search />} />       */}
+          <Route path="/search/:searchId" element={<Search />} />      
         </Routes>
       </Container>
       {/* <Footer /> */}
