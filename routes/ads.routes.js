f81629593385dad236f756f8c15d304fc03567de
Pawel Godula <bolomaster7@gmail.com>
2023-05-15 17:10:15 +0200

    minor changes

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -6,7 +6,7 @@ const imageUpload = require('../utils/imageUpload');
 
 router.get('/ads', adsController.getAll);
 router.get('/ads/:id', adsController.getById);
-router.post('/ads', authMiddleware, adsController.post);
+router.post('/ads', authMiddleware, adsController.post, imageUpload.single('image'));
 router.delete('/ads/:id', authMiddleware, adsController.delete);
 router.put('/ads/:id', authMiddleware, adsController.edit);
 router.put('/ads/:id', authMiddleware,imageUpload.single('image'),adsController.edit);
