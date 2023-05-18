dda6ebe9806cd21c788bd3165627a305948775e6
Pawel Godula <bolomaster7@gmail.com>
2023-05-18 19:09:59 +0200

    footer and header fix

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import styles from './Footer.module.scss'
 
 const Footer = () => {
-  return <div>footer</div>;
-};
+  return <div className={styles.footer}>Copyright &copy;Pablo || 2023</div>
+}
 
-export default Footer;
+export default Footer
--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -1,56 +1,68 @@
 import styles from './Header.module.scss';
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getUser } from '../../../redux/userRedux';
 const Header = () => {
+  const user = useSelector(getUser);
+
   return (
-    <nav>
-      <div className={styles.nav_wrapper}>
-        <NavLink to="/" className={styles.logo}>
-          <h2>Notice Board</h2>
-        </NavLink>
-        <ul className={styles.nav_list}>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? styles.linkActive : undefined
-              }
-              to="/"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? styles.linkActive : undefined
-              }
-              to='/login'
-            >
-              Sign in
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? styles.linkActive : undefined
-              }
-              to='/register'
-            >
-              Register
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? styles.linkActive : undefined
-              }
-              to='/logout'
-            >
-              Logout
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav>
+        <div className={styles.nav_wrapper}>
+          <NavLink to="/" className={styles.logo}>
+            <h2>Notice Board</h2>
+          </NavLink>
+          <ul className={styles.nav_list}>
+            <li>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? styles.linkActive : undefined
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              {!user && (
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? styles.linkActive : undefined
+                  }
+                  to="/login"
+                >
+                  Sign in
+                </NavLink>
+              )}
+            </li>
+            <li>
+              {!user && (
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? styles.linkActive : undefined
+                  }
+                  to="/register"
+                >
+                  Register
+                </NavLink>
+              )}
+            </li>
+            <li>
+              {user && (
+                <NavLink
+                  className={({ isActive }) =>
+                    isActive ? styles.linkActive : undefined
+                  }
+                  to="/logout"
+                >
+                  Logout
+                </NavLink>
+              )}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
